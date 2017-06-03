1a098d7bbb4b19edca7b3bdaa0558c8803e08e8d
Chun F Hsu <smalloshin@gmail.com>
2017-06-03 23:25:10 +0800

    add sudo

--- v1.0/api-start.sh
+++ v1.0/api-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "***** BUILD DOCKER IMAGES *****"
-docker-compose up --build -d
+sudo docker-compose up --build -d
 echo "***** LOAD DEFAULT DATA *****"
-docker exec mirror-related-news-api python feed_to_redis.py
+sudo docker exec mirror-related-news-api python feed_to_redis.py
