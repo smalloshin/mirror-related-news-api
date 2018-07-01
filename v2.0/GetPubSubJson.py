e37d5f1f410f58091ddd3ca7daf9a8033df621ae
smalloshin <smalloshin@gmail.com>
2018-07-01 13:33:56 +0000

    modify the logic. Some tasks should be done later.

--- v2.0/GetPubSubJson.py
+++ v2.0/GetPubSubJson.py
@@ -2,6 +2,12 @@ import os
 from google.cloud import pubsub
 import json
 import ConfigParser
+import jieba
+from util.ContentParser import *
+import cPickle as Pickle
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from BuildIndexTreeV2 import *
 
 # read the config for redis connection
 config = ConfigParser.ConfigParser()
@@ -9,13 +15,23 @@ config.read('related-news-engine.conf')
 credential = config.get('PUBSUB','GOOGLE_APPLICATION_CREDENTIALS')
 project_id = config.get('PUBSUB','PROJECT_ID')
 topic = config.get('PUBSUB','TOPIC')
-sub = 'sub_test142'
+sub = 'sub_test13'
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential
 
 subscriber = pubsub.SubscriberClient()
 topic_name = 'projects/{project_id}/topics/{topic}'.format(project_id=project_id,topic=topic)
 subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(project_id=project_id,sub=sub)
+
+# preload the models
+# 1. load jieba
+jieba.load_userdict('dict/moe.dict')
+jieba.analyse.set_stop_words('dict/stopping_words.dict')
+# 2. compute tfidf feature
+cv = Pickle.load(open(pkl_dir+"cv.pkl","r"))
+# 3. index 
+cp = Pickle.load(open(dest_dir+'pysparnn-index.pkl','r'))
+
 
 try:
     subscriber.create_subscription(subscription_name, topic_name)
