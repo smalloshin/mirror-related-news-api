bf0da5dfa00961a3749ebc7cd3a3fbd50145c0e6
smalloshin <smalloshin@gmail.com>
2018-07-01 13:18:59 +0000

    switch to V2

--- v2.0/daily_operation.py
+++ v2.0/daily_operation.py
@@ -4,10 +4,20 @@ from GetFeatureVectors import *
 from BuildIndexTree import *
 from FeedToRedis import *
 import time
+import os
+
 
 t = time.time()
 print("######## Related News Engine V2 ########")
 print("Daily Operation Starts...")
+print("**** Step 0: Preparing directories ****")
+if os.path.isdir('streaming-data/')==False:
+    os.makedirs('streaming-data/')
+else:
+    import glob
+    filelist=glob.glob(os.path.join('streaming-data/', "*"))
+    for f in filelist:
+        os.remove(f)
 print("**** Step 1: Crawling json files for all news articles ****")
 CrawlRawJson()
 print("\n **** Step 2: Extract TF-IDF from articles ****")
@@ -15,7 +25,7 @@ ExtractTFIDF()
 print("\n ****  Step 3: Generate Feature Vectors for articles ****")
 fv, id_list = GetFeatureVectors()
 print("\n **** Step 4: Build index tree for related news ****")
-BuildIndexTree(fv,id_list)
+BuildIndexTreeV2(fv,id_list)
 print("\n **** Step 5: Feed related news articles into Redis ****")
-FeedToRedis()
+FeedToRedisV2()
 print(" ######## Done! Spent: "+str(time.time()-t)+"(s) ########")
